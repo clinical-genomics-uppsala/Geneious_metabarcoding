c32651d6b30dfcfd493eef0a57ec1129b7aa5459
ikarls <write.ida@gmail.com>
2023-02-10 13:26:17 +0100

    run multiple samples from disk

--- emu_linux_mac.py
+++ emu_linux_mac.py
@@ -6,38 +6,37 @@ import subprocess
 import shutil
 import glob
 
-# Geneious input/output and options
-inFile = os.path.join("/geneious", sys.argv[2])
-outFile = os.path.join("/geneious", sys.argv[4])
+# Outfile to be imported to Geneious
+outFile = sys.argv[2]
 
 # Temporary Geneious folder. Example: /Users/user/Geneious 2022.1 Data/transient/1660719270002/x/8/
-pathToGeneiousData = sys.argv[6]
-mountPath = os.path.join(pathToGeneiousData, ":/geneious")
+pathToGeneiousData = sys.argv[4]
 
-pathToDocker = sys.argv[8]
-pathToOutData = sys.argv[10]
+pathToDocker = sys.argv[6]
 
-# Sample path
-pathToSample = sys.argv[12]
+# In/out data folder selected by user
+pathToData = sys.argv[8]
+mountPath = os.path.join(pathToData, ":/geneious")
 
-# Run emu docker container
-subprocess.run( [pathToDocker, "run", "--rm", "-v", mountPath, "emu3.4.4_image", \
-    "emu", "abundance", inFile, \
-    "--db", "/tmp/geneious/emu-database", \
-    "--keep-counts", "--keep-files", "--keep-read-assignments", "--output-unclassified", \
-    "--output-dir", "/geneious"] )
+# List of fasta files in data folder
+inFiles = []
+for file in os.listdir(pathToData):
+    if file.endswith(".fasta"):
+        inFiles.append(os.path.join("/geneious",file))
 
+# Run emu abundance for each sample
+for inFile in inFiles:
+    subprocess.run( [pathToDocker, "run", "--rm", "-v", mountPath, "emu3.4.4_image", \
+        "emu", "abundance", inFile, \
+        "--db", "/tmp/geneious/emu-database", \
+        "--keep-counts", "--keep-files", "--keep-read-assignments", "--output-unclassified", \
+        "--output-dir", "/geneious"] )
 
-# Get sample name & clean sample dir
-for f in os.listdir(pathToSample):
-    if ".sample" in f:
-        sample_name = f
-        os.remove(os.path.join(pathToSample,f))
+# Run emu combine-outputs for selected folder - both relative abundance and counts
+combineOutputs = ("emu combine-outputs /geneious species; emu combine-outputs --counts /geneious species")
 
+subprocess.run( [pathToDocker, "run", "--rm" ,"-v", mountPath, \
+    "emu3.4.4_image", "/bin/bash", "-c", combineOutputs] )
 
-# Rename files - replace "input." with sample name
-for file_name in os.listdir(pathToGeneiousData):
-    if ".fastq.gz" not in file_name:
-        if "input." in file_name:
-            new_name = sample_name + "_" + file_name.split("input.")[1]
-            shutil.copyfile(file_name, os.path.join(pathToOutData, new_name))
+# Copy combined output file to Geneious tmp folder
+shutil.copyfile(os.path.join(pathToData, outFile), os.path.join(pathToGeneiousData, outFile))
