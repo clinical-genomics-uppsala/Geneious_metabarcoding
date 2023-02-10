c1b90575de558ebbb2dc3b86cc508ccd909ffca9
ikarls <write.ida@gmail.com>
2023-02-10 09:53:02 +0100

    with sample name plugin

--- emu_linux_mac.py
+++ emu_linux_mac.py
@@ -4,6 +4,7 @@ import sys
 import os
 import subprocess
 import shutil
+import glob
 
 # Geneious input/output and options
 inFile = os.path.join("/geneious", sys.argv[2])
@@ -16,6 +17,9 @@ mountPath = os.path.join(pathToGeneiousData, ":/geneious")
 pathToDocker = sys.argv[8]
 pathToOutData = sys.argv[10]
 
+# Sample path
+pathToSample = sys.argv[12]
+
 # Run emu docker container
 subprocess.run( [pathToDocker, "run", "--rm", "-v", mountPath, "emu3.4.4_image", \
     "emu", "abundance", inFile, \
@@ -24,6 +28,16 @@ subprocess.run( [pathToDocker, "run", "--rm", "-v", mountPath, "emu3.4.4_image",
     "--output-dir", "/geneious"] )
 
 
-# Copy outfiles to user selected folder
-shutil.copytree(pathToGeneiousData, pathToOutData, ignore=shutil.ignore_patterns('*.fastq.gz'), dirs_exist_ok=True)
+# Get sample name & clean sample dir
+for f in os.listdir(pathToSample):
+    if ".sample" in f:
+        sample_name = f
+        os.remove(os.path.join(pathToSample,f))
 
+
+# Rename files - replace "input." with sample name
+for file_name in os.listdir(pathToGeneiousData):
+    if ".fastq.gz" not in file_name:
+        if "input." in file_name:
+            new_name = sample_name + "_" + file_name.split("input.")[1]
+            shutil.copyfile(file_name, os.path.join(pathToOutData, new_name))
