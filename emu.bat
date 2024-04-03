a4b7645371a7d6fdbc8827f4064ee79bda51669b
ikarls <write.ida@gmail.com>
2024-04-03 14:18:04 +0200

    image and threads for windows

--- emu.bat
+++ emu.bat
@@ -1,3 +1,6 @@
+:: Path to current directory to bundled optional files
+set pluginPath=%~dp0
+
 :: Output to Geneious
 set outFile=%2
 
@@ -5,11 +8,12 @@ set outFile=%2
 set pathToGeneiousData=%4
 set pathToDocker=%6
 set pathToData=%8
-set emuImage=%10
-set noThreads=%12
-
-:: Path to current directory to bundled optional files
-set pluginPath=%~dp0
+shift
+shift
+shift
+shift
+set emuImage=%6
+set noThreads=%8
 
 python "%pluginPath%emu.py" -o %outFile% -g %pathToGeneiousData% -d %pathToDocker% -f %pathToData% -i %emuImage% -t %noThreads%
 
