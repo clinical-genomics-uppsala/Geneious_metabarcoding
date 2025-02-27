362c6744ff88037906fdd6cef16facf01e4b97f9
ikarls <write.ida@gmail.com>
2025-02-27 11:51:34 +0100

    feat: implement config for windows

--- emu_wrapper/emu.bat
+++ emu_wrapper/emu.bat
@@ -6,21 +6,10 @@ set outFile=%2
 
 :: Options from Geneious
 set pathToGeneiousData=%4
-set pathToDocker=%6
+set configFile=%6
 set pathToData=%8
-shift
-shift
-shift
-shift
-shift
-shift
-set gitVersion=%4
-set kronaImage=%6
-set emuImage=%8
-shift
-shift
-set noThreads=%8
 
-python "%pluginPath%emu.py" -o %outFile% -g %pathToGeneiousData% -d %pathToDocker% -f %pathToData% -v %gitVersion% -k %kronaImage% -i %emuImage% -t %noThreads%
 
-:: -o emu_output.tsv -g [inputFolderName] [otherOptions]
+python "%pluginPath%emu.py" -o %outFile% -g %pathToGeneiousData% -p %configFile% -f %pathToData%
+
+:: Wrapper Plugin Creator command line: -o emu_output.tsv -g [inputFolderName] [otherOptions]
